33d3a3495486c37370d9ff10e427e9582e05204e
zencore <dobetter@zencore.cn>
2018-03-15 20:08:37 +0800

    fix deps

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ with open(os.path.join(here, 'README.rst'), "r", encoding="utf-8") as fobj:
     long_description = fobj.read()
 
 requires = [
-    "click",
+    "django",
+    "django_click",
+    "redis",
 ]
 
 setup(
